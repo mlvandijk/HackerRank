3de660b3d87fe26e341d1b7fe2ea8c7093ed1086
Marit <mlvandijk@users.noreply.github.com>
2016-10-25 20:17:09 +0200

    Solution for HackerRank CompareTriplets

---
+++ CompareTriplets.java
@@ -0,0 +1,26 @@
+/**
+ * Solution for Compare the triplets: https://www.hackerrank.com/challenges/compare-the-triplets
+ * Created on 13-09-2016
+ * @author Marit van Dijk
+ */
+
+package algorithms;
+
+import java.util.*;
+
+public class CompareTriplets {
+
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int a0 = in.nextInt();
+	        int a1 = in.nextInt();
+	        int a2 = in.nextInt();
+	        int b0 = in.nextInt();
+	        int b1 = in.nextInt();
+	        int b2 = in.nextInt();
+	        
+	        int pointsAlice = ((a0>b0)?1:0)+ ((a1>b1)?1:0)+ ((a2>b2)?1:0) ;
+	        int pointsBob = ((a0<b0)?1:0)+ ((a1<b1)?1:0)+ ((a2<b2)?1:0) ;
+	        System.out.println(pointsAlice+" " +pointsBob);
+	    }
+	}
